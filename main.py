453b97648fe220d4b02bbfae2cface3c33ef640b
lucastoll <lucasfernandestolotto@gmail.com>
2025-05-18 18:48:04 -0300

    feat: add .env

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from models.request_model import UserRequest
 from agents.router_agent import RouterAgent
+from dotenv import load_dotenv
 import json
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=[os.getenv('CORS_ORIGIN')], 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"], 
